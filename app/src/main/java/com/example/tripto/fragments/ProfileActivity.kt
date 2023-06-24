435b6c8372c1fa3d33b2b88045c0cc89cc9c9663
reemehab <60987184+reemehab@users.noreply.github.com>
2023-06-24 06:47:55 +0300

    profile updates

--- app/src/main/java/com/example/tripto/fragments/ProfileActivity.kt
+++ app/src/main/java/com/example/tripto/fragments/ProfileActivity.kt
@@ -1,10 +1,6 @@
 package com.example.tripto.fragments
 
-import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.fragment.app.Fragment
@@ -12,45 +8,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.Toast
+import com.example.tripto.EditProfileActivity
+import com.example.tripto.HomeActivity
 import com.example.tripto.R
 class ProfileActivity : Fragment() {
-    private val SELECT_IMAGE_REQUEST = 1
-    private lateinit var profileImageView: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.activity_profile, container, false)
-
-        // Find the ImageView and Button in your layout
-        profileImageView = view.findViewById(R.id.profileImageView)
-        val selectImageButton: Button = view.findViewById(R.id.selectImageButton)
-
-        // Set click listener for the select image button
-        selectImageButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            startActivityForResult(intent, SELECT_IMAGE_REQUEST)
+           val bt_editProfile_click = view.findViewById<Button>(R.id.edit_profile)
+        bt_editProfile_click.setOnClickListener {
+            val intent = Intent(requireContext(), EditProfileActivity::class.java)
+            startActivity(intent)
         }
+
 
         return view
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == SELECT_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
-            val imageUri: Uri? = data.data
-            try {
-                val bitmap = MediaStore.Images.Media.getBitmap(requireContext().contentResolver, imageUri)
-                val scaledBitmap = Bitmap.createScaledBitmap(bitmap, profileImageView.width, profileImageView.height, false)
-                val drawable = BitmapDrawable(resources, scaledBitmap)
-                profileImageView.setImageDrawable(drawable)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-    }
 
 
 }
